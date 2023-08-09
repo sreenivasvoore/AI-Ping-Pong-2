bff7a53abf907be6b461ea93515fecb32b5793ef
sreenivasvoore <vlns88@gmail.com>
2023-08-08 19:49:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ var ball = {
 
 function setup(){
   var canvas = createCanvas(700, 600);
-  canvas.parent();
+  canvas.parent('canvas');
   video = createCapture(VIDEO);
   video.size(700, 600);
   video.hide();
